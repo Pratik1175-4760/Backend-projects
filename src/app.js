3187cdc58ee9b786a38c87b316bc3a882203b16a
Pratik1175-4760 <pratikshivajiyadav@gmail.com>
2026-01-26 19:56:19 +0530

    Added Util Components

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+// This are all middlewares which are used for handling configurations between server and requests
+
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true
+}))
+// This is used for allowing requests and to provide response to and from desired web page url only like from www.example.com is our website so only from this not from google or amazon etc.
+
+app.use(express.json({limit:"16kb"}))
+// TO allow json format requests from req and we can put limit of the json file 
+
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+// As url can be encoded in many formats so we tell express to allow or understand url of different formats
+
+app.use(express.static("public")) // To use this folder for all wherever needed
+
+app.use(cookieParser()) // To parse cookies of browsers
+
+export {app} 
--- src/index.js
+++ src/index.js
@@ -4,4 +4,20 @@ dotenv.config({
 })
 import express from "express"
 import connectDB from "./db/index.js";
-connectDB();
+import { app } from "./app.js";
+
+
+connectDB()
+.then(()=>{
+  app.on("error", (error)=>{
+    console.log("error: \n", error);
+    throw error;
+  })
+  app.listen(process.env.PORT || 8000, ()=>
+  {
+    console.log(`Server is running at ${process.env.PORT}`)
+  })
+})
+.catch((err)=>{
+  console.log("Mongo bd connection failed: \n", err)
+})
